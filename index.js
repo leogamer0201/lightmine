76da4e56d720a323d34ef6dc0a4c810dcff53d3a
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-09-22 17:05:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,15 +26,11 @@ bot.on("message", function(message) {
 			.addField('Comandos Públicos:', '!serverinfo - Mostra as informações do servidor\n!reportar - Reporta um usuário para a Staff')
 			.setColor('#ff7a00')
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
-			
-			const h2 = new Discord.RichEmbed()
+
 			.addField('Comandos para Moderação:', '!ban - Bane o usuário do servidor(Banir Membros)\n!kick - Expulsa o usuário do servidor(Expulsar Membros)')
-			.setColor('#ff1800')
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
 			
-			const h3 = new Discord.RichEmbed()
 			.addField('Outros Comandos:', '!anunciar - Faz um anúncio no canal #anuncios(Gerenciar Canais)')
-			.setColor('#00ff01')
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
 	
 			  try{
@@ -43,20 +39,6 @@ bot.on("message", function(message) {
     console.log(e.stack);
     message.channel.send(`${message.author}**, Habilite o Dm para eu enviar os comandos.**`)
   }
-		
-		  try{
-    await message.author.send(h2)
-  }catch(e){
-    console.log(e.stack);
-  }
-		
-				  try{
-    await message.author.send(h3)
-  }catch(e){
-    console.log(e.stack);
-  }
-		
-		}
 
         if (command == `${prefix}serverinfo`) {
             const embed = new Discord.RichEmbed()
